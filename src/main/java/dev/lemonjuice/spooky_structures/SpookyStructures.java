55a892eadf212714e312c1a9144016a45f05165e
Lemon-Juiced <47193272+Lemon-Juiced@users.noreply.github.com>
2024-09-30 19:15:11 -0400

    General set up stuff

---
+++ src/main/java/dev/lemonjuice/spooky_structures/SpookyStructures.java
@@ -0,0 +1,39 @@
+package dev.lemonjuice.spooky_structures;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+
+@Mod(SpookyStructures.MODID)
+public class SpookyStructures {
+    public static final String MODID = "spooky_structures";
+
+    public SpookyStructures(IEventBus modEventBus, ModContainer modContainer) {
+        modEventBus.addListener(this::commonSetup);
+
+        NeoForge.EVENT_BUS.register(this);
+
+        modEventBus.addListener(this::addCreative);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {}
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {}
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {}
+
+    @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {}
+    }
+}
