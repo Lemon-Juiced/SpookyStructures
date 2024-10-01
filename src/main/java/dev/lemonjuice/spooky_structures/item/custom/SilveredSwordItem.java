c5fdaf2cc527ee832fc7bf94e29d39e633265a1a
Lemon-Juiced <47193272+Lemon-Juiced@users.noreply.github.com>
2024-10-01 01:04:09 -0400

    More reliance on Scalar Core for methods that can be static / reusable

--- src/main/java/dev/lemonjuice/spooky_structures/item/custom/SilveredSwordItem.java
+++ src/main/java/dev/lemonjuice/spooky_structures/item/custom/SilveredSwordItem.java
@@ -1,33 +1,16 @@
 package dev.lemonjuice.spooky_structures.item.custom;
 
-import com.mojang.datafixers.util.Either;
 import net.minecraft.ChatFormatting;
-import net.minecraft.core.Holder;
-import net.minecraft.core.HolderOwner;
-import net.minecraft.core.RegistryAccess;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.TagKey;
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.damagesource.DamageSources;
-import net.minecraft.world.damagesource.DamageType;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.animal.horse.SkeletonHorse;
-import net.minecraft.world.entity.animal.horse.ZombieHorse;
-import net.minecraft.world.entity.boss.wither.WitherBoss;
-import net.minecraft.world.entity.monster.*;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.TooltipFlag;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Predicate;
-import java.util.stream.Stream;
+
+import static dev.lemonjuice.scalar_core.category.Category.isUndeadEntity;
 
 public class SilveredSwordItem extends SwordItem {
 
@@ -47,13 +30,4 @@ public class SilveredSwordItem extends SwordItem {
         tooltipComponents.add(component);
     }
 
-    private boolean isUndeadEntity(LivingEntity entity) {
-        return (entity instanceof Phantom
-                || entity instanceof AbstractSkeleton // Bogged, Stray, Wither Skeleton
-                || entity instanceof SkeletonHorse
-                || entity instanceof WitherBoss
-                || entity instanceof Zoglin
-                || entity instanceof Zombie // Drowned, Husk, Zombie Villager, Zombified Piglin
-                || entity instanceof ZombieHorse);
-    }
 }
